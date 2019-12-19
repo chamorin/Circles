e8c9690c05d1b2e51c82d78736d7435c6f9f15a4
Charles Morin <morin.charles.5@courrier.uqam.ca>
2019-12-19 14:29:06 -0500

    Game: Added center circle

--- src/game.c
+++ src/game.c
@@ -24,8 +24,10 @@ void Game_run(struct Game *game) {
   float originX = SCREEN_WIDTH / 2;
   float originY = SCREEN_HEIGHT / 2;
 
-  game->player_sprite_posX = originX + cos(angle) * CENTER_CIRCLE_RADIUS;
-  game->player_sprite_posY = originY + sin(angle) * CENTER_CIRCLE_RADIUS;
+  game->player_sprite_posX =
+      (originX + cos(angle) * CENTER_CIRCLE_RADIUS) - PLAYER_WIDTH / 2;
+  game->player_sprite_posY =
+      (originY + sin(angle) * CENTER_CIRCLE_RADIUS) - PLAYER_HEIGHT / 2;
 
   while (game->state != GAME_OVER) {
     while (SDL_PollEvent(&e) != 0) {
@@ -42,7 +44,12 @@ void Game_run(struct Game *game) {
     }
     SDL_SetRenderDrawColor(game->renderer, 0x00, 0x00, 0x00, 0x00);
     SDL_RenderClear(game->renderer);
+    SDL_Point point;
+    point.x = originX;
+    point.y = originY;
+    SDL_Color color = {128, 128, 128};
     Spritesheet_render(game->background, BACKGROUND_X, BACKGROUND_Y, 255, 0);
+    Game_drawCircle(point, game->renderer, CENTER_CIRCLE_RADIUS - 20, color);
     Spritesheet_render(game->player_sprite, game->player_sprite_posX,
                        game->player_sprite_posY, 255, 0);
     SDL_RenderPresent(game->renderer);
@@ -60,8 +67,10 @@ void Game_delete(struct Game *game) {
 void Player_move_right(struct Game *game, const float *originX,
                        const float *originY, float *angle) {
   *angle += PLAYER_MOVEMENT_ANGLE;
-  game->player_sprite_posX = *originX + cos(*angle) * CENTER_CIRCLE_RADIUS;
-  game->player_sprite_posY = *originY + sin(*angle) * CENTER_CIRCLE_RADIUS;
+  game->player_sprite_posX =
+      (*originX + cos(*angle) * CENTER_CIRCLE_RADIUS) - PLAYER_WIDTH / 2;
+  game->player_sprite_posY =
+      (*originY + sin(*angle) * CENTER_CIRCLE_RADIUS) - PLAYER_HEIGHT / 2;
   if (game->player_position == PLAYER_POSSIBLE_POSITIONS - 1) {
     game->player_position = 0;
   } else {
@@ -72,11 +81,27 @@ void Player_move_right(struct Game *game, const float *originX,
 void Player_move_left(struct Game *game, const float *originX,
                       const float *originY, float *angle) {
   *angle -= PLAYER_MOVEMENT_ANGLE;
-  game->player_sprite_posX = *originX + cos(*angle) * CENTER_CIRCLE_RADIUS;
-  game->player_sprite_posY = *originY + sin(*angle) * CENTER_CIRCLE_RADIUS;
+  game->player_sprite_posX =
+      (*originX + cos(*angle) * CENTER_CIRCLE_RADIUS) - PLAYER_WIDTH / 2;
+  game->player_sprite_posY =
+      (*originY + sin(*angle) * CENTER_CIRCLE_RADIUS) - PLAYER_HEIGHT / 2;
   if (game->player_position == 0) {
     game->player_position = PLAYER_POSSIBLE_POSITIONS - 1;
   } else {
     --game->player_position;
+  }
+}
+
+void Game_drawCircle(SDL_Point center, SDL_Renderer *renderer, int radius,
+                     SDL_Color color) {
+  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+  for (int w = 0; w < radius * 2; ++w) {
+    for (int h = 0; h < radius * 2; ++h) {
+      int dx = radius - w;
+      int dy = radius - h;
+      if ((dx * dx + dy * dy) <= (radius * radius)) {
+        SDL_RenderDrawPoint(renderer, center.x + dx, center.y + dy);
+      }
+    }
   }
 }
